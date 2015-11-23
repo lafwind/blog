a44e289efcbe0c5eb2e44114c9178c8d58ae05e0
Lafwind <lafwind@gmail.com>
2015-11-23 23:20:14 +0800

    UI 改版(第二版): 完成 blog 首页改版

--- config.rb
+++ config.rb
@@ -14,7 +14,7 @@ activate :blog do |blog|
   # This will add a prefix to all links, template references and source paths
   # blog.prefix = "blog"
 
-  blog.permalink = "/:title.html"
+  blog.permalink = "/blog/:title.html"
   blog.sources = "/posts/:year-:month-:day-:title.html"
   # blog.permalink = "{year}/{month}/{day}/{title}.html"
   # Matcher for blog source files
