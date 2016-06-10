91f02ef5bc48d2f4b6b153c0910d6084e37d9c68
Lafwind <lafwind@gmail.com>
2016-06-11 00:09:14 +0800

    完成 blog 主页的响应式设计

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,7 +1,11 @@
 //= require_tree .
 
 $(document).ready(function(){
-  $('.arrow').click(function(){
-    $("html, body").animate({scrollTop: $('.content').offset().top}, 600);
-  });
-})
+    $('.arrow').click(function(){
+        $("html, body").animate({scrollTop: $('.content').offset().top}, 600);
+    });
+
+    $('.handle').on('click', function() {
+        $('nav').toggleClass('show');
+    });
+});
