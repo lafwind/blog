6d75053658fe289f0e2b0fbb4841fca160c718c6
Lafwind <lafwind@gmail.com>
2016-07-09 20:46:44 +0800

    添加 project 页面相关动画

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,9 +1,18 @@
 //= require_tree .
 
-$(document).ready(function(){
-    $('.arrow').click(function(){
-        $("html, body").animate({scrollTop: $('.content').offset().top}, 600);
+function click_go_to (target) {
+    $('.to_' + target).on('click', function(e){
+        e.preventDefault();
+        $("html, body").animate({scrollTop: $('.' + target).offset().top}, 600);
     });
+}
+
+$(document).ready(function(){
+    click_go_to('content');
+    click_go_to('top');
+    click_go_to('p1');
+    click_go_to('p2');
+    click_go_to('footer');
 
     $('.handle').on('click', function() {
         $('nav').toggleClass('show');
