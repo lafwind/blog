0f231333afe14331c13010178e0e4699fd635533
Lafwind <lafwind@gmail.com>
2016-07-10 20:42:03 +0800

    project 页面添加 top button 并实现相关动画

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -7,12 +7,35 @@ function click_go_to (target) {
     });
 }
 
+function toggleBottomClass() {
+    var offset_to_bottom = $(document).height() - $(window).scrollTop();
+
+    // ele min-height: 400
+    if ( offset_to_bottom <= 400 || offset_to_bottom <= $(window).height()) {
+        $('.nav.top_button').addClass('bottom');
+    } else {
+        $('.nav.top_button').removeClass('bottom');
+    }
+}
+
 $(document).ready(function(){
     click_go_to('content');
     click_go_to('top');
     click_go_to('p1');
     click_go_to('p2');
     click_go_to('footer');
+
+    $(window).scroll( function() {
+        // ele min-height: 400
+        var offset_to_top = $(window).height() > 400 ? $(window).height() : 400;
+        if ($(window).scrollTop() >= offset_to_top) {
+            toggleBottomClass();
+            $('.top_button').fadeIn(200);
+        } else {
+            $('.top_button').fadeOut(200);
+        }
+
+    });
 
     $('.handle').on('click', function() {
         $('nav').toggleClass('show');
